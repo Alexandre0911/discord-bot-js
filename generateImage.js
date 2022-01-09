73dc597a6eff98e1373ec4fa376611e7177edddb
Alexandre <71990615+Alexandre0911@users.noreply.github.com>
2022-01-09 09:36:34 +0000

    New Bot Function

---
+++ generateImage.js
@@ -0,0 +1,13 @@
+const canvas = requires("canvas")
+
+const bg = ""
+
+const av = {
+    size: 256
+}
+
+const generateImage = async (member) => {
+    let username = member.user.username
+    let discrim = member.user.discriminator
+    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size: av.size})
+}
--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const Discord = require("discord.js")
+
+var status = require("minecraft-server-status")
+
 require("dotenv").config()
 
 
 
+const welcomeChannelId = "929633937779728414"
+
+const minecraft = "929639886464614400"
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
-
 
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
-
 
 
 client.on("messageCreate", (message) => {
@@ -25,11 +31,31 @@ client.on("messageCreate", (message) => {
 })
 
 
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`Hey, <@${member.id}>, welcome to the server!`)
+})
 
 
 
+client.on("messageCreate", (message) => {
 
+    if (message.content.toLowerCase() == '!server'){
 
+        status('alexandre2206.ddns.net', 25565, response => {
+            console.log(response.online)
+            console.log(response.players.now)
+
+        if (response.online){
+            message.reply(`Server is online and there are ${response.players.now} players playing right now!`)
+        }
+        
+        else{
+            message.reply("Server is closed!")
+        }
+        
+    })
+    }
+})
 
 
 
